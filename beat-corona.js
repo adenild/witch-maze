4f17062d732fdefec087cebb490df75c2e12a07f
adenild <adenildojunior52@gmail.com>
2020-11-16 07:34:34 -0300

    :sparkles: Modularizei as funções que criam e que contam a pontuação das recopensas no grid.
    Apaguei a pasta do vscode
    
    Atualizei ids e classes de tags que estavam em português
    
    Apaguei a maioria dos comentários
    
    Apaguei a maioria dos console.logs

--- beat-corona.js
+++ beat-corona.js
@@ -5,19 +5,17 @@ let mazeHeight;
 let mazeWidth;
 let player;
 
-
 class Player {
-
   constructor(moves) {
     this.reset();
     this.startMoves = moves;
     this.moves = moves;
   }
   reset() {
-    this.col = 0;
-    this.row = 0;
+    this.col = Math.floor(Math.random() * 10);
+    this.row = Math.floor(Math.random() * 10);
     this.moves = this.startMoves;
-    $('.moves-left').text(this.moves);
+    $('#movesLeft').text(this.moves);
   }
 
   moveUp() {
@@ -25,53 +23,41 @@ class Player {
       if (!maze.cells[this.col][this.row].northWall && this.row !== 0) {
         this.row -= 1;
         this.moves -= 1;
-        console.log("moves:", this.moves);
-        console.log(this.col, this.row);
-        $('.moves-left').text(this.moves);
+        $('#movesLeft').text(this.moves);
       }
-    } else { alert("Sem movimentos restantes! Deseja comprar mais?") }
+    } else {alert("Sem movimentos restantes! Deseja comprar mais?")}
   }
-
   moveDown() {
     if (this.moves > 0) {
       if (!maze.cells[this.col][this.row].southWall && this.row !== maze.rows - 1) {
         this.row += 1;
         this.moves -= 1;
-        console.log("moves:", this.moves);
-        console.log(this.col, this.row);
-        $('.moves-left').text(this.moves);
+        $('#movesLeft').text(this.moves);
       }
-    } else { alert("Sem movimentos restantes! Deseja comprar mais?") }
+    } else {alert("Sem movimentos restantes! Deseja comprar mais?")}
   }
-
   moveLeft() {
     if (this.moves > 0) {
       if (!maze.cells[this.col][this.row].westWall && this.col !== 0) {
         this.col -= 1;
         this.moves -= 1;
-        console.log("moves:", this.moves);
-        console.log(this.col, this.row);
-        $('.moves-left').text(this.moves);
+        $('#movesLeft').text(this.moves);
       }
-    } else { alert("Sem movimentos restantes! Deseja comprar mais?") }
+    } else {alert("Sem movimentos restantes! Deseja comprar mais?")}
   }
-
   moveRight() {
     if (this.moves > 0) {
       if (!maze.cells[this.col][this.row].eastWall && this.col !== maze.cols - 1) {
         this.col += 1;
         this.moves -= 1;
-        console.log("moves:", this.moves);
-        console.log(this.col, this.row);
-        $('.moves-left').text(this.moves);
+        $('#movesLeft').text(this.moves);
       }
-    } else { alert("Sem movimentos restantes! Deseja comprar mais?") }
+    } else {alert("Sem movimentos restantes! Deseja comprar mais?")}
   }
 
 }
 
 class MazeCell {
-
   constructor(col, row) {
     this.col = col;
     this.row = row;
@@ -81,35 +67,27 @@ class MazeCell {
     this.southWall = false;
     this.westWall = false;
   }
-
 }
 
 class Maze {
-
   constructor(cols, rows, cellSize, map) {
+    this.reset();
+
+    this.cols = cols;
+    this.rows = rows;
+    this.cellSize = cellSize;
+    this.cells = [];
+    this.map = $.csv.toObjects(map);
 
     this.backgroundColor = "#ffffff";
-    this.cols = cols;
     this.endColor = "#88FF88";
     this.mazeColor = "#000000";
     this.playerColor = "#880088";
-    this.rows = rows;
-    this.cellSize = cellSize;
-    this.newRewards = true; // -----------------------------------------------------------------------------------------------
-    this.rewardsCoords = []; // -----------------------------------------------------------------------------------------------
-    this.rewards = 0;
-    this.nivel = 1;
-
-    this.cells = [];
-
-    this.map = $.csv.toObjects(map);
 
     this.generate()
-
   }
 
   async generate() {
-
     mazeHeight = this.rows * this.cellSize;
     mazeWidth = this.cols * this.cellSize;
 
@@ -118,27 +96,10 @@ class Maze {
     canvas.style.height = mazeHeight;
     canvas.style.width = mazeWidth;
 
-    console.log(this.map[17])
-    // let map_index = 0;
     for (let col = 0; col < this.cols; col++) {
       this.cells[col] = [];
       for (let row = 0; row < this.rows; row++) {
         this.cells[col][row] = new MazeCell(col, row);
-
-        // if (this.map[map_index].EastWall == 1) {
-        //   this.cells[col][row].eastWall = true;
-        // }
-        // if (this.map[map_index].NorthWall == 1) {
-        //   this.cells[col][row].northWall = true;
-        // }
-        // if (this.map[map_index].SouthWall == 1) {
-        //   this.cells[col][row].southWall = true;
-        // }
-        // if (this.map[map_index].WestWall == 1) {
-        //   this.cells[col][row].westWall = true;
-        // }
-        //console.log(this.map[map_index].EastWall)
-        // map_index += 1;
       }
     }
 
@@ -155,12 +116,7 @@ class Maze {
         if (this.map[map_index].WestWall == 1) {
           this.cells[this.map[map_index].Col-1][this.map[map_index].Row-1].westWall = true;
         }
-        //console.log(this.map[map_index].EastWall)
-
       }
-    
-
-
 
     let rndCol = Math.floor(Math.random() * this.cols);
     let rndRow = Math.floor(Math.random() * this.rows);
@@ -169,47 +125,52 @@ class Maze {
     stack.push(this.cells[rndCol][rndRow]);
 
     this.redraw();
+  }
 
+  spawnRewards() {
+    if (this.newRewards) {
+      let cont = 0;
+      while (cont < (this.level*2)) {
+        let randomCol = Math.floor(Math.random() * this.cols);
+        let randomRow = Math.floor(Math.random() * this.rows);
+        // Checa se o jogador está na casa, para nao colocar uma recompensa lá
+        if (player.col !== randomCol || player.row !== randomRow) {
+          this.rewardsList.push([randomCol, randomRow]);
+          ctx.fillRect((randomCol)*this.cellSize+5, (randomRow)*this.cellSize+5, this.cellSize-5, this.cellSize-5);
+          cont += 1;
+        }
+      }
+      this.newRewards = false;
+    } else {
+      for (let r = 0; r < this.rewardsList.length; r++) {
+        ctx.fillRect((this.rewardsList[r][0])*this.cellSize+5, (this.rewardsList[r][1])*this.cellSize+5, this.cellSize-5, this.cellSize-5);
+      }
+    }
+  }
+
+  countScore() {
+    for (let r = 0; r < this.rewardsList.length; r++) {
+      if (player.col === this.rewardsList[r][0] && player.row === this.rewardsList[r][1]) {
+        this.rewardsScore += 1;
+        $("#rewardsScore").text(this.rewardsScore);
+        this.rewardsList.splice(r, 1);
+        if (this.rewardsList.length === 0) {
+          this.newRewards = true;
+          this.level += 1;
+          $("#level").text(this.level);
+        }
+      }
+    }
   }
 
   redraw() {
-
     ctx.fillStyle = this.backgroundColor;
     ctx.fillRect(0, 0, mazeHeight, mazeWidth);
 
     ctx.fillStyle = this.endColor;
 
-    for (let i = 0; i < this.rewardsCoords.length; i++) {
-      if (player.col == this.rewardsCoords[i][0] && player.row == this.rewardsCoords[i][1]) {
-        this.rewards += 1;
-        $('.rewards').text(this.rewards);
-        console.log(this.rewards);
-        this.rewardsCoords.splice(i, 1);
-        if (this.rewardsCoords.length == 0) {
-          this.newRewards = true;
-          this.nivel += 1;
-          $('.nivel').text(this.nivel);
-        }
-      }
-    }
-
-    if (this.newRewards) {
-      let count = 0;
-      while (count < (this.nivel*2)) {
-        let rndCol = Math.floor(Math.random() * this.cols);
-        let rndRow = Math.floor(Math.random() * this.rows);
-        if (player.col != rndCol || player.row != rndRow) {
-          this.rewardsCoords.push([rndCol, rndRow]);
-          ctx.fillRect((rndCol) * this.cellSize + 5, (rndRow) * this.cellSize + 5, this.cellSize - 5, this.cellSize - 5);
-          count += 1;
-        }
-      }
-      this.newRewards = false
-    } else {
-      for (let i = 0; i < this.rewardsCoords.length; i++) {
-        ctx.fillRect((this.rewardsCoords[i][0]) * this.cellSize + 5, (this.rewardsCoords[i][1]) * this.cellSize + 5, this.cellSize - 5, this.cellSize - 5);
-      }
-    }
+    this.countScore()
+    this.spawnRewards()
 
     ctx.strokeStyle = this.mazeColor;
     ctx.strokeRect(0, 0, mazeHeight, mazeWidth);
@@ -245,13 +206,21 @@ class Maze {
 
     ctx.fillStyle = this.playerColor;
     ctx.fillRect((player.col * this.cellSize) + 5, (player.row * this.cellSize) + 5, this.cellSize - 5, this.cellSize - 5);
-
   }
 
+  reset() {
+    this.newRewards = true;
+    this.rewardsList = [];
+    this.rewardsScore = 0;
+    this.level = 1;
+    $("#level").text(this.level);
+    $("#rewardsScore").text(this.rewardsScore);
+  }
 }
 
 function onClick() {
   player.reset();
+  maze.reset();
   maze.cols = document.getElementById("cols").value;
   maze.rows = document.getElementById("rows").value;
   maze.generate();
@@ -307,9 +276,10 @@ async function onLoad() {
   ctx = canvas.getContext('2d');
 
   player = new Player(200);
-  $('.moves-left').text(player.moves)
+  // $('#movesLeft').text(player.moves)
 
   maze = new Maze(10, 10, 50, await obtem_csv());
+
 
   document.addEventListener('keydown', onKeyDown);
   document.getElementById('generate').addEventListener('click', onClick);
@@ -330,9 +300,7 @@ async function obtem_csv(){
 }
 
 async function csv_novo(allText){
-  let data = $.csv.toObjects(allText);
-  //console.log(data)
-  return data
+  return $.csv.toObjects(allText)
 }
 
 function maze_generator(maze_data,maze){
