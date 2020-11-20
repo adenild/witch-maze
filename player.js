e165740e4da3226f7dc4866a1a4f381a2aa419da
Saulo Leão <sleaojr@gmail.com>
2020-11-20 18:47:21 -0300

    :recycle: adicionado um moveHandler para captura de movimentos

--- player.js
+++ player.js
@@ -11,40 +11,36 @@ class Player {
         $('#movesLeft').text(this.moves);
     }
 
-    moveUp() {
+    moveHandler(direction) {
         if (this.moves > 0) {
+            this[direction]();
+            this.moves -= 1;
+            $('#movesLeft').text(this.moves);
+            maze.redraw();
+        } else {
+            alert('Sem movimentos restantes! Deseja jogar novamente?')
+        }
+    }
+
+    up() {
             if (!maze.cells[this.col][this.row].northWall && this.row !== 0) {
                 this.row -= 1;
-                this.moves -= 1;
-                $('#movesLeft').text(this.moves);
             }
-        } else {alert("Sem movimentos restantes! Deseja jogar novamente?")}
-    }
-    moveDown() {
-        if (this.moves > 0) {
+        }
+
+    down() {
             if (!maze.cells[this.col][this.row].southWall && this.row !== maze.rows - 1) {
                 this.row += 1;
-                this.moves -= 1;
-                $('#movesLeft').text(this.moves);
             }
-        } else {alert("Sem movimentos restantes! Deseja jogar novamente?")}
     }
-    moveLeft() {
-        if (this.moves > 0) {
+    left() {
             if (!maze.cells[this.col][this.row].westWall && this.col !== 0) {
                 this.col -= 1;
-                this.moves -= 1;
-                $('#movesLeft').text(this.moves);
             }
-        } else {alert("Sem movimentos restantes! Deseja jogar novamente?")}
     }
-    moveRight() {
-        if (this.moves > 0) {
+    right() {
             if (!maze.cells[this.col][this.row].eastWall && this.col !== maze.cols - 1) {
                 this.col += 1;
-                this.moves -= 1;
-                $('#movesLeft').text(this.moves);
             }
-        } else {alert("Sem movimentos restantes! Deseja jogar novamente?")}
     }
 }
