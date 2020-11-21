02173bf06450b5ea1ab4c26f79cf8fcb76e86f84
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-11-21 15:08:14 -0300

    add: adicionada primeira implementação swipe
    Signed-off-by: Thiago Luis <thiagoluisrpinho@gmail.com>

--- beat-corona.js
+++ beat-corona.js
@@ -54,9 +54,55 @@ async function onLoad() {
   maze = new Maze(10, 10, 50, await obtem_csv());
 
   document.addEventListener('keydown', onKeyDown);
+  document.addEventListener('touchstart', handleTouchStart, false);        
+  document.addEventListener('touchmove', handleTouchMove, false);
   document.getElementById('generate').addEventListener('click', onClick);
   document.getElementById('up').addEventListener('click', onControlClick);
   document.getElementById('right').addEventListener('click', onControlClick);
   document.getElementById('down').addEventListener('click', onControlClick);
   document.getElementById('left').addEventListener('click', onControlClick);
 }
+
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            player.moveHandler("left")
+        } else {
+            player.moveHandler("right")
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            player.moveHandler("up")
+        } else { 
+            player.moveHandler("down")
+        }                                                                 
+    }
+    maze.redraw();
+    xDown = null;
+    yDown = null;                                             
+};
