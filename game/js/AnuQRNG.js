001d254f24f7f9a309c3255e3669cd480ceb9821
Thiago Luis <thiagoluisrpinho@gmail.com>
2020-12-22 13:30:31 -0300

    :zap: refactor: fazendo requisições a cada 1024 números gerados

--- game/js/AnuQRNG.js
+++ game/js/AnuQRNG.js
@@ -1,13 +1,21 @@
 class AnuQRNG {
     constructor(seed) {
         this.seed = seed;
+        this.url = "https://qrng.anu.edu.au/API/jsonI.php?length=1024&type=uint16";
+        this.random_numbers = this.get_numbers();
     }
     random() {
-        var url = "https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint16";
+      if (this.random_numbers.length == 0) {
+        this.random_numbers = this.get_numbers();
+      }
+      return this.random_numbers.pop()/65535;
+    }
+
+    get_numbers() {
         var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", url, false); // false for synchronous request
+        xmlHttp.open("GET", this.url, false); // false for synchronous request
         xmlHttp.send(null);
         var obj = JSON.parse(xmlHttp.responseText);
-        return obj.data.pop()/65535;
+        return obj.data;
     }
 }
