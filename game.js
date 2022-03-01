e528bbc04b9668126633d9171628fd93b5bf06eb
Saulo Leão <sleaojr@gmail.com>
2022-03-01 20:57:56 -0300

    :avocado: come bacate, bem

--- game.js
+++ game.js
@@ -13,183 +13,208 @@ let x_down = null;
 let y_down = null;
 let userData;
 let userCookie;
-let start_time_swipe, finish_time_swipe
+let start_time_swipe, finish_time_swipe;
 let mapa;
 
-function generateRandomBetween(n,a,b,method) {
-
-    let numbersList = [];
-    if (method == "Mersenne") {
-        for (let i = 1; i <= n; ++i) {
-            numbersList.push(parseFloat((a + randomModule2.random() * (b-a)).toFixed(3)));
-        }
+function generateRandomBetween(n, a, b, method) {
+  let numbersList = [];
+  if (method == "Mersenne") {
+    for (let i = 1; i <= n; ++i) {
+      numbersList.push(
+        parseFloat((a + randomModule2.random() * (b - a)).toFixed(3))
+      );
     }
-    else { // Quantico
-        return randomModuleQuant.random(player.isBot);
-    }
+  } else {
+    // Quantico
+    return randomModuleQuant.random(player.isBot);
+  }
 
-    return numbersList
+  return numbersList;
 }
 
 function getCookie() {
-    if (document.cookie.indexOf('user_id') == -1) {
-        document.cookie = 'user_id=' + String(seed);
-    }
-    return document.cookie
+  if (document.cookie.indexOf("user_id") == -1) {
+    document.cookie = "user_id=" + String(seed);
+  }
+  return document.cookie;
 }
 
 function getTouches(event) {
-    return event.touches ||          // browser API
-        event.originalEvent.touches; // jQuery
+  return (
+    event.touches || // browser API
+    event.originalEvent.touches
+  ); // jQuery
 }
 
 function handleTouchStart(event) {
-    const firstTouch = getTouches(event)[0];
-    x_down = firstTouch.clientX;
-    y_down = firstTouch.clientY;
-    start_time_swipe = new Date().getTime();
+  const firstTouch = getTouches(event)[0];
+  x_down = firstTouch.clientX;
+  y_down = firstTouch.clientY;
+  start_time_swipe = new Date().getTime();
 }
 
 function handleTouchMove(event) {
-    if (!x_down || !y_down) {
-        return;
-    }
+  if (!x_down || !y_down) {
+    return;
+  }
 
-    let x_up = event.touches[0].clientX;
-    let y_up = event.touches[0].clientY;
-    finish_time_swipe = new Date().getTime();
+  let x_up = event.touches[0].clientX;
+  let y_up = event.touches[0].clientY;
+  finish_time_swipe = new Date().getTime();
 
-    let x_diff = x_down - x_up;
-    let y_diff = y_down - y_up;
+  let x_diff = x_down - x_up;
+  let y_diff = y_down - y_up;
 
-    //x_up,y_up,x_down,y_down
-    let diff_swipe_time = finish_time_swipe - start_time_swipe
-    let move_data = [x_down , y_down, x_up, y_up, diff_swipe_time];
-    let move_type = 'swipe';
+  //x_up,y_up,x_down,y_down
+  let diff_swipe_time = finish_time_swipe - start_time_swipe;
+  let move_data = [x_down, y_down, x_up, y_up, diff_swipe_time];
+  let move_type = "swipe";
 
-    if (Math.abs(x_diff) > Math.abs(y_diff)) {/*most significant*/
-        if (x_diff > 0) {
-            player.moveHandler("left", move_data, move_type)
-        } else {
-            player.moveHandler("right", move_data, move_type)
-        }
+  if (Math.abs(x_diff) > Math.abs(y_diff)) {
+    /*most significant*/
+    if (x_diff > 0) {
+      player.moveHandler("left", move_data, move_type);
     } else {
-        if (y_diff > 0) {
-            player.moveHandler("up", move_data, move_type)
-        } else {
-            player.moveHandler("down", move_data, move_type)
-        }
+      player.moveHandler("right", move_data, move_type);
     }
-    x_down = null;
-    y_down = null;
+  } else {
+    if (y_diff > 0) {
+      player.moveHandler("up", move_data, move_type);
+    } else {
+      player.moveHandler("down", move_data, move_type);
+    }
+  }
+  x_down = null;
+  y_down = null;
 }
 
 function onKeyDown(event) {
-    switch (event.keyCode) {
-        case 37:
-        case 65:
-            player.moveHandler("left")
-            break;
-        case 39:
-        case 68:
-            player.moveHandler("right")
-            break;
-        case 40:
-        case 83:
-            player.moveHandler("down")
-            break;
-        case 38:
-        case 87:
-            player.moveHandler("up")
-            break;
-        default:
-            break;
+  switch (event.keyCode) {
+    case 37:
+    case 65:
+      player.moveHandler("left");
+      break;
+    case 39:
+    case 68:
+      player.moveHandler("right");
+      break;
+    case 40:
+    case 83:
+      player.moveHandler("down");
+      break;
+    case 38:
+    case 87:
+      player.moveHandler("up");
+      break;
+    default:
+      break;
+  }
+}
+
+class EasyCookies {
+  constructor() {
+    this.cookies = this.loadCookies();
+  }
+
+  loadCookies() {
+    let cookies = {};
+    if (document.cookie) {
+      document.cookie
+        .split(";")
+        .map((x) => x.split("="))
+        .map((x) => (cookies[x[0].trim()] = x[1].trim()));
     }
+    return cookies;
+  }
+
+  get(key) {
+    return this.cookies[key];
+  }
+
+  set(key, value) {
+    this.cookies[key] = value;
+    document.cookie = `${key}=${value}`;
+  }
+}
+
+function choose_random_method(easycookies) {
+  let choices = ["MERSENNE", "ANUQRNG"];
+  let prev = easycookies.get("previousMethod");
+  if (prev) {
+    choices.splice(choices.indexOf(prev), 1);
+  }
+  let choice = choices[Math.floor(choices.length * Math.random())];
+  easycookies.set("previousMethod", choice);
+  return choice;
 }
 
 async function onLoad() {
-    seed = new Date().getTime();
-    seed2 = new Date().getTime()+(Math.random() * (9 - 1) + 1);
+  seed = new Date().getTime();
+  seed2 = new Date().getTime() + (Math.random() * (9 - 1) + 1);
 
-    randomModule = new MersenneTwister(seed);
-    randomModule2 = new MersenneTwister(seed2);
-    randomModuleQuant = new AnuQRNG(seed);
-    userCookie = getCookie()
+  randomModule = new MersenneTwister(seed);
+  randomModule2 = new MersenneTwister(seed2);
+  randomModuleQuant = new AnuQRNG(seed);
+  cookies = new EasyCookies();
+  userCookie = cookies.get("user_id");
+  if (!userCookie) {
+    userCookie = seed;
+  }
+  console.log(userCookie);
+  method = choose_random_method(cookies);
 
-    let method_index = userCookie.indexOf('previous_method')
-    if (method_index == -1) {
-        if (Math.round(Math.random()) == 0){
-            method = 'ANUQRNG';
-        }
-        else {
-            method = 'Mersenne';
-        }
-    } else {
-        let previous_method = userCookie.split(';')
-            .find(element => element.includes('previous_method'))
-            .replace(' previous_method=', '')
-        if (previous_method == 'ANUQRNG') {
-            method = 'Mersenne'
-        } else if (previous_method == 'Mersenne') {
-            method = 'ANUQRNG'
-        }
-    }
-    document.cookie = `previous_method=${method}`
+  userData = new UserData(seed, method, userCookie);
+  userData.setDataStructure();
 
-    userData = new UserData(seed, method, userCookie);
-    userData.setDataStructure();
+  canvas = document.getElementById("mainForm");
+  ctx = canvas.getContext("2d");
 
-    canvas = document.getElementById('mainForm');
-    ctx = canvas.getContext('2d');
+  player = new Player(240);
 
-    player = new Player(240);
+  await player.loadPlayerImage();
+  $("#movesLeft").text(player.moves);
 
-    await player.loadPlayerImage();
-    $('#movesLeft').text(player.moves)
+  reward = new Reward();
 
-    reward = new Reward();
+  mapa = await obtem_csv();
+  maze = new Maze(10, 10, 50, mapa);
 
-    mapa = await obtem_csv();
-    maze = new Maze(10, 10, 50, mapa);
-
-    document.addEventListener('keydown', onKeyDown);
-    document.addEventListener('touchstart', handleTouchStart, false);
-    document.addEventListener('touchmove', handleTouchMove, false);
+  document.addEventListener("keydown", onKeyDown);
+  document.addEventListener("touchstart", handleTouchStart, false);
+  document.addEventListener("touchmove", handleTouchMove, false);
 }
 
 function loadReplay(oldData) {
-    seed = oldData['seed'];
-    method = oldData['method'];
+  seed = oldData["seed"];
+  method = oldData["method"];
 
-    randomModule = new MersenneTwister(seed);
-    randomModuleQuant = new AnuQRNG(seed,controle='controle');
+  randomModule = new MersenneTwister(seed);
+  randomModuleQuant = new AnuQRNG(seed, (controle = "controle"));
 
-    userCookie = oldData['userCookie'];
-    moves = oldData.userDict['round']['direction'];
+  userCookie = oldData["userCookie"];
+  moves = oldData.userDict["round"]["direction"];
 
-    userData = new UserData(seed, method, userCookie);
-    userData.setDataStructure('controle');
-    
-    
-    canvas = document.getElementById('mainForm');
-    ctx = canvas.getContext('2d');
+  userData = new UserData(seed, method, userCookie);
+  userData.setDataStructure("controle");
 
-    player = new Player(240, true);
-    player.loadPlayerImage();
-    // $('#movesLeft').text(player.moves);
-    if (player.isBot) {
-        //console.log("verifiquei se é bot e é memo")
-        reward = new Reward(true);
-    } else {
-        reward = new Reward();
-    }
+  canvas = document.getElementById("mainForm");
+  ctx = canvas.getContext("2d");
 
+  player = new Player(240, true);
+  player.loadPlayerImage();
+  // $('#movesLeft').text(player.moves);
+  if (player.isBot) {
+    //console.log("verifiquei se é bot e é memo")
+    reward = new Reward(true);
+  } else {
+    reward = new Reward();
+  }
 
-    maze = new Maze(10, 10, 50, mapa);
+  maze = new Maze(10, 10, 50, mapa);
 
-    player.control_simulator(moves);
-    // console.log(reward.magicScore)
-    // document.addEventListener('keydown', onKeyDown);
-    // document.addEventListener('touchstart', handleTouchStart, false);
-    // document.addEventListener('touchmove', handleTouchMove, false);
+  player.control_simulator(moves);
+  // console.log(reward.magicScore)
+  // document.addEventListener('keydown', onKeyDown);
+  // document.addEventListener('touchstart', handleTouchStart, false);
+  // document.addEventListener('touchmove', handleTouchMove, false);
 }
